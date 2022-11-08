6a0686dd8feaa028dadf4b622d761587289b6a82
josep.lopez <josep.lopez@col.vueling.com>
2022-11-08 14:28:12 +0100

    Interface Ready

--- ios_01_simplecalculator_uri/ViewController.swift
+++ ios_01_simplecalculator_uri/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfNum2: UITextField!
+    @IBOutlet weak var tfNum1: UITextField!
+    @IBOutlet weak var btnSuma: UIButton!
+    @IBOutlet weak var btnResta: UIButton!
+    @IBOutlet weak var lblResultat: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
